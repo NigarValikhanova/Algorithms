071623da9fd000cd273e89dc73bbf6ad6a6d8f31
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2024-05-31 11:10:49 +0400

    Add files via upload
    2511. Maximum Enemy Forts That Can Be Captured

---
+++ LeetCodeAlgorithms/LeetCodeAlgorithms/LeetCode2511/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgorithms.LeetCode2511
+{
+    public class Solution
+    {
+        public int CaptureForts(int[] forts)
+        {
+            int n = forts.Length;
+            int maxCaptured = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (forts[i] == 1)
+                {
+                    // Check to the right
+                    int captured = 0;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (forts[j] == 0)
+                        {
+                            captured++;
+                        }
+                        else if (forts[j] == -1)
+                        {
+                            maxCaptured = Math.Max(maxCaptured, captured);
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    // Check to the left
+                    captured = 0;
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        if (forts[j] == 0)
+                        {
+                            captured++;
+                        }
+                        else if (forts[j] == -1)
+                        {
+                            maxCaptured = Math.Max(maxCaptured, captured);
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return maxCaptured;
+        }
+    }
+}
---
+++ LeetCodeAlgorithms/LeetCodeAlgorithms/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeAlgorithms.LeetCode2511;
+
+Console.WriteLine("Hello, World!");
+
+int[] forts = [0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, 0, 1];
+
+Solution solution = new Solution();
+
+Console.WriteLine(solution.CaptureForts(forts));
